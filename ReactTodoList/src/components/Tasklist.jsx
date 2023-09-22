2bc4d9c0d5fb16520d848a1244f99a86d3915525
gjlander <garrett.jlander@gmail.com>
2023-09-22 18:39:23 +0200

    imported tabs and moved tasklist into one-currently hardcoded and no way to add new tasklist

--- ReactTodoList/src/components/Tasklist.jsx
+++ ReactTodoList/src/components/Tasklist.jsx
@@ -1,39 +1,44 @@
-import { useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import AddNewTodo from "./AddNewTodo";
 import TodoItem from "./TodoItem";
 function Tasklist({ title, setTitle, handleTitleText, allTodos, setAllTodos }) {
     return (
         <div className="row justify-content-center">
             <div className="col col-lg-6">
-                <h2 className="display-6">Tasks</h2>
-                <AddNewTodo
-                    title={title}
-                    setTitle={setTitle}
-                    handleTitleText={handleTitleText}
-                    allTodos={allTodos}
-                    setAllTodos={setAllTodos}
-                />
-                <ul className="list-group bg-light h-100 taskList">
-                    {allTodos &&
-                        allTodos.map((todo) => (
-                            <TodoItem
-                                key={todo.id}
-                                {...todo}
-                                allTodos={allTodos}
-                                setAllTodos={setAllTodos}
-                                // editing={editing}
-                                // setEditing={setEditing}
-                            />
-                        ))}
-                    {/* <TodoItem
-                        title={title}
-                        setTitle={setTitle}
-                        allTodos={allTodos}
-                        setAllTodos={setAllTodos}
-                        editing={editing}
-                        setEditing={setEditing}
-                    /> */}
-                </ul>
+                <Tabs>
+                    <TabList>
+                        <Tab>Tasks</Tab>
+                        <Tab>Title 2</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <h2 className="display-6">Tasks</h2>
+                        <AddNewTodo
+                            title={title}
+                            setTitle={setTitle}
+                            handleTitleText={handleTitleText}
+                            allTodos={allTodos}
+                            setAllTodos={setAllTodos}
+                        />
+                        <ul className="list-group bg-light h-100 taskList">
+                            {allTodos &&
+                                allTodos.map((todo) => (
+                                    <TodoItem
+                                        key={todo.id}
+                                        {...todo}
+                                        allTodos={allTodos}
+                                        setAllTodos={setAllTodos}
+                                        // editing={editing}
+                                        // setEditing={setEditing}
+                                    />
+                                ))}
+                        </ul>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Any content 2</h2>
+                    </TabPanel>
+                </Tabs>
             </div>
         </div>
     );
