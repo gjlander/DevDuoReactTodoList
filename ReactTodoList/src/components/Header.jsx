1256c67b6e5d5a6cb175189e19a51376f093bfaa
Catia <wolfgangcatia@gmail.com>
2023-09-25 13:23:51 +0200

    minimal styling play-around

--- ReactTodoList/src/components/Header.jsx
+++ ReactTodoList/src/components/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
     return (
         <div className="row justify-content-center">
             <div className="col-8">
-                <h1 className="display-4 bg-primary fw-bold rounded p-3">
-                    To Do List
+                <h1 className="display-4 bg-info fw-bold rounded p-3">
+                    To Duos
                 </h1>
             </div>
         </div>
